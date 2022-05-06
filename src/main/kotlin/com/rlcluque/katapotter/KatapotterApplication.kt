f9ebf9ec2d941922cbf4177421320d20cab1f202
Rafa Luque <rlcluque@gmail.com>
2022-05-07 01:05:12 +0200

    Initial commit

---
+++ src/main/kotlin/com/rlcluque/katapotter/KatapotterApplication.kt
@@ -0,0 +1,11 @@
+package com.rlcluque.katapotter
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KatapotterApplication
+
+fun main(args: Array<String>) {
+    runApplication<KatapotterApplication>(*args)
+}
---
+++ src/test/kotlin/com/rlcluque/katapotter/KatapotterApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.rlcluque.katapotter
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KatapotterApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
